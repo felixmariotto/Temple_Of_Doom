f12811bd7a5b45de6a43c2dc4617214a08fe0889
felix <felixmariotto@gmail.com>
2019-08-14 22:28:37 +0800

    create a helper that display lines in the scene according to the atlas

--- public/js/Atlas.js
+++ public/js/Atlas.js
@@ -3,11 +3,11 @@ function Atlas() {
 
 	// ATLAS ARRAYS
 	// these arrays hold the height of individual steps in the track
-	const d0 = [ 0, 0, 0, 1, 1, 0, 0, 0 ];
-	const d1 = [ 1, 1, 0, 0, 1, 2, 1, 2 ];
-	const d2 = [ 0, 0, 0, 1, 2, 3, 0, 1 ];
+	const d0 = [ 0, 0, 0, 1, 1, 0, 0, 0 ]; // RED
+	const d1 = [ 1, 1, 0, 0, 1, 2, 1, 2 ]; // BLUE
+	const d2 = [ 0, 0, 0, 1, 2, 3, 0, 1 ]; // GREEN
 
-	var points = [];
+	var pointsArrays = [];
 
 	// initVectors() transforms the atlas arrays (d0, d1 and d2),
 	// into arrays of THREE.Vector3.
@@ -15,17 +15,16 @@ function Atlas() {
 
 		[ d0, d1, d2 ].forEach( (dimension, i)=> {
 
-			points.push([]);
+			pointsArrays.push([]);
 
-			// transform the atlas references into points,
+			// transform the atlas references into pointsArrays,
 			// the atlas hold only the height, the horizontal offset
 			// is equal to the position in the array.
 			dimension.forEach( (height, index)=> {
-				points[i].push( new THREE.Vector3( index, height, 0 ) );
+				pointsArrays[i].push( new THREE.Vector3( index, height, i ) );
 			});
 
 		});
-		console.log( points );
 	};
 
 
@@ -42,7 +41,8 @@ function Atlas() {
 
 	return {
 		getIDs,
-		initVectors
+		initVectors,
+		pointsArrays
 	};
 
 };
--- public/js/AtlasHelper.js
+++ public/js/AtlasHelper.js
@@ -1,9 +1,41 @@
 
-function AtlasHelper( scene ) {
+function AtlasHelper( pointsArrays ) {
 
-	
+	let group = new THREE.Group();
+	let scaleVec = new THREE.Vector3( 1, 0, 0 );
 
-	return {
+
+
+	pointsArrays.forEach( (points, i)=> {
+
+		switch (i) {
+			case 0 : color = 'red'; break;
+			case 1 : color = 'blue'; break;
+			case 2 : color = 'green'; break;
+		};
+
+		var material = new THREE.LineBasicMaterial({
+			color: color
+		});
+
+		var geometry = new THREE.Geometry();
+
+		points.forEach( (vec)=> {
+
+			geometry.vertices.push(
+				vec,
+				new THREE.Vector3()
+					.copy(vec)
+					.add(scaleVec)
+			);
+
+		})
 		
-	};
+
+		var line = new THREE.Line( geometry, material );
+		group.add( line );
+
+	})
+
+	return group ;
 };
