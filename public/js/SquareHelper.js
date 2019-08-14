ef01574132995919337d271796601f5c6215ad65
felix <felixmariotto@gmail.com>
2019-08-14 23:04:27 +0800

    finish squareHelper

--- public/js/SquareHelper.js
+++ public/js/SquareHelper.js
@@ -1,7 +1,24 @@
 
 function SquareHelper( logicSquare ) {
 
-	console.log( logicSquare );
+	let group = new THREE.Group();
+	group.position = logicSquare.position;
 
-	return true;
+	var material = new THREE.LineBasicMaterial({
+		color: 0xffffff
+	});
+
+	var geometry = new THREE.Geometry();
+
+	geometry.vertices.push(
+		new THREE.Vector3( logicSquare.height, 0, 0 ),
+		new THREE.Vector3( logicSquare.height, logicSquare.width, 0 ),
+		new THREE.Vector3( 0, logicSquare.width, 0 ),
+		new THREE.Vector3( 0, 0, 0 ),
+	);
+	
+	var line = new THREE.Line( geometry, material );
+	group.add( line );
+
+	return group;
 };
