5db7086d5f489c12f4ce825c9e64f24196e31f48
felix <felixmariotto@gmail.com>
2019-08-14 22:02:33 +0800

    put initVectors() into Atlas.js

--- public/js/Atlas.js
+++ public/js/Atlas.js
@@ -1,11 +1,34 @@
 
 function Atlas() {
 
+	// ATLAS ARRAYS
+	// these arrays hold the height of individual steps in the track
 	const d0 = [ 0, 0, 0, 1, 1, 0, 0, 0 ];
-
 	const d1 = [ 1, 1, 0, 0, 1, 2, 1, 2 ];
-
 	const d2 = [ 0, 0, 0, 1, 2, 3, 0, 1 ];
+
+	var points = [];
+
+	// initVectors() transforms the atlas arrays (d0, d1 and d2),
+	// into arrays of THREE.Vector3.
+	function initVectors() {
+
+		[ d0, d1, d2 ].forEach( (dimension, i)=> {
+
+			points.push([]);
+
+			// transform the atlas references into points,
+			// the atlas hold only the height, the horizontal offset
+			// is equal to the position in the array.
+			dimension.forEach( (height, index)=> {
+				points[i].push( new THREE.Vector3( index, height, 0 ) );
+			});
+
+		});
+		console.log( points );
+	};
+
+
 
 	function getIDs( d ) {
 		switch (d) {
@@ -15,14 +38,11 @@ function Atlas() {
 		};
 	};
 
-	function getArrays() {
-		return [ d0, d1, d2 ];
-	};
+
 
 	return {
 		getIDs,
-		getArrays,
-		prout:10
+		initVectors
 	};
 
 };
--- public/js/AtlasHelper.js
+++ public/js/AtlasHelper.js
@@ -1,19 +1,9 @@
 
 function AtlasHelper( scene ) {
 
-	var points = [];
-
-	function init( IDs ) {
-		IDs.forEach( (dimension, i)=> {
-			points.push([]);
-			dimension.forEach( (height, index)=> {
-				points[i].push( new THREE.Vector3( index, height, 0 ) );
-			});
-		});
-		console.log( points );
-	};
+	
 
 	return {
-		init
+		
 	};
 };
