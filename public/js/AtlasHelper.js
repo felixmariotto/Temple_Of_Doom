1f2e55d6e6ff8b8955d56872a3a428046a8564b0
felix <felixmariotto@gmail.com>
2019-08-14 21:56:10 +0800

    add a function to transform the steps in points

--- public/js/AtlasHelper.js
+++ public/js/AtlasHelper.js
@@ -1,10 +1,16 @@
 
 function AtlasHelper( scene ) {
 
-
+	var points = [];
 
 	function init( IDs ) {
-		console.log( IDs );
+		IDs.forEach( (dimension, i)=> {
+			points.push([]);
+			dimension.forEach( (height, index)=> {
+				points[i].push( new THREE.Vector3( index, height, 0 ) );
+			});
+		});
+		console.log( points );
 	};
 
 	return {
