b86e885a1b4ea20185f226cac942e59381eb6067
Felix Mariotto <felixmariotto@gmail.com>
2019-08-15 12:59:19 +0800

    create a keystate module

---
+++ public/js/Keys.js
@@ -0,0 +1,58 @@
+
+function Keys() {
+
+	var isPressed = {
+		'up': false,
+		'down': false,
+		'left': false,
+		'right': false,
+		'space': false
+	};
+
+
+
+	function init() {
+		document.addEventListener('keydown', (e)=> {
+			onKeyChange( e, true );
+		});
+		document.addEventListener('keyup', (e)=> {
+			onKeyChange( e, false );
+		});
+	};
+
+
+	function onKeyChange( e, bool ) {
+
+		switch ( e.key ) {
+
+			case "ArrowRight" :
+				isPressed.right = bool ;
+			break;
+
+			case "ArrowLeft" :
+				isPressed.left = bool ;
+			break;
+
+			case "ArrowUp" :
+				isPressed.up = bool ;
+			break;
+
+			case "ArrowDown" :
+				isPressed.down = bool ;
+			break;
+
+			case " " :
+				isPressed.space = bool ;
+			break;
+		};
+
+		console.log( isPressed );
+	};
+
+
+
+	return {
+		init,
+		isPressed
+	};
+};
