86bdf98a61e1f6bc42de396113e3e5664cfb9a80
felix <felixmariotto@gmail.com>
2019-08-15 22:15:37 +0800

    create a chaser track, to have it go up and down while it's chasing the player

--- public/js/Chaser.js
+++ public/js/Chaser.js
@@ -5,7 +5,8 @@ function Chaser( needHelper ) {
 	const SPEED = 0.03 ;
 
 	var params = {
-		position: -5
+		position: 0,
+		isRunning: false
 	};
 
 	var group = new THREE.Group();
@@ -14,21 +15,34 @@ function Chaser( needHelper ) {
 
 
 	if ( needHelper ) {
-		var geometry = new THREE.BoxBufferGeometry( 2, 100, 2.3 );
+		var geometry = new THREE.BoxBufferGeometry( 4, 7, 2.3 );
 		var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
 		var cube = new THREE.Mesh( geometry, material );
 		group.add( cube );
 	};
 
 
+
 	function update() {
-		params.position += SPEED ;
-		group.position.x = params.position ;
+		if ( params.isRunning ) {
+			params.position += SPEED ;
+			group.position.x = params.position ;
+			group.position.y = THREE.Math.lerp(
+				atlas.chaserTrack[ Math.floor(group.position.x) ],
+				atlas.chaserTrack[ Math.ceil(group.position.x) ],
+				group.position.x - Math.floor(group.position.x) );
+		};
+	};
+
+
+	function start() {
+		params.isRunning = true;
 	};
 
 
 	return {
-		update
+		update,
+		start
 	};
 
 };
